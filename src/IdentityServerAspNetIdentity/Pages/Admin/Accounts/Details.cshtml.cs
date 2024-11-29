d4672c18583832ac03dc563c19b930b29ec62f8e
ColmKenna <colmkenna@gmail.com>
2024-11-29 16:33:28 +0000

    Add blank details page for a user

---
+++ src/IdentityServerAspNetIdentity/Pages/Admin/Accounts/Details.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IdentityServerAspNetIdentity.Pages.Admin.Accounts;
+
+public class Details : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
