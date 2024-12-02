b2c2f0f37864690b1ed84ede1bdd847c939bef9d
ColmKenna <colmkenna@gmail.com>
2024-12-02 15:20:30 +0000

    Create account details page

--- src/IdentityServerAspNetIdentity/Pages/Admin/Accounts/Details.cshtml.cs
+++ src/IdentityServerAspNetIdentity/Pages/Admin/Accounts/Details.cshtml.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using IdentityDataModels;
+using IdentityServer.EF.DataAccess;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace IdentityServerAspNetIdentity.Pages.Admin.Accounts;
 
 public class Details : PageModel
 {
-    public void OnGet()
+    IUserRepository userRepository;
+
+    public Details(IUserRepository userRepository)
     {
-        
+        this.userRepository = userRepository;
+    }
+    
+    public ApplicationUserDetails ApplicationUserDetails { get; set; }
+    public async Task OnGetAsync(string id)
+    {
+        ApplicationUserDetails = await userRepository.GetUserById(id);
     }
 }
