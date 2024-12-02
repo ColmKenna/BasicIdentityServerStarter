00921ea927ed1162c912a4e3271c605b8ac60a01
ColmKenna <colmkenna@gmail.com>
2024-12-02 15:17:33 +0000

    Add taghelper for field content with scss

---
+++ src/IdentityServerAspNetIdentity/TagHelpers/Fields/FieldContainerTagHelper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace IdentityServerAspNetIdentity.TagHelpers.Fields;
+
+[HtmlTargetElement("field-container", Attributes = "label, value")]
+public class FieldContainerTagHelper : TagHelper
+{
+    public string Label { get; set; }
+    public string Value { get; set; }
+
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+        output.TagName = "p"; // Replace the original tag with a <p> tag
+        output.TagMode = TagMode.StartTagAndEndTag; // Ensure it renders a complete tag
+        output.Attributes.SetAttribute("class", "field-container");
+
+        // Set the content
+        output.Content.SetHtmlContent($@"
+                <strong>{Label}</strong> {Value}
+            ");
+    }
+}
