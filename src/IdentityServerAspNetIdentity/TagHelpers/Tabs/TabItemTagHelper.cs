78ae5745bc0226efee2d8c3f24d8db1ce38baf68
ColmKenna <colmkenna@gmail.com>
2024-12-02 15:16:53 +0000

    Add taghelper and css and js for tabs

---
+++ src/IdentityServerAspNetIdentity/TagHelpers/Tabs/TabItemTagHelper.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace IdentityServerAspNetIdentity.TagHelpers.Tabs;
+
+[HtmlTargetElement("tab-item", ParentTag = "tab")]
+public class TabItemTagHelper : TagHelper
+{
+    public string Header { get; set; }
+    public string TabId { get; set; }
+
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+        // Generate header and content
+        var headerHtml = $@"
+            <button class='tab-link' data-ck-tab-target='{TabId}'>{Header}</button>";
+        var contentHtml = $@"
+            <div id='{TabId}' class='tab'>
+                <div class='tab-content'>
+                    {output.GetChildContentAsync().Result.GetContent()}
+                </div>
+            </div>";
+
+        // Append header and content to the parent's context
+        if (context.Items.TryGetValue("TabHeaders", out var headersObj) && headersObj is List<string> headers)
+        {
+            headers.Add(headerHtml);
+        }
+
+        if (context.Items.TryGetValue("TabContents", out var contentsObj) && contentsObj is List<string> contents)
+        {
+            contents.Add(contentHtml);
+        }
+
+        // Suppress output as it will be managed by the parent
+        output.SuppressOutput();
+    }
+}
---
+++ src/IdentityServerAspNetIdentity/TagHelpers/Tabs/TabTagHelper.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace IdentityServerAspNetIdentity.TagHelpers.Tabs;
+
+
+[HtmlTargetElement("tab")]
+public class TabTagHelper : TagHelper
+{
+    public string Id { get; set; }
+
+    public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+    {
+        // Initialize shared context for headers and contents
+        context.Items["TabHeaders"] = new List<string>();
+        context.Items["TabContents"] = new List<string>();
+
+        // Process child tags
+        var childContent = await output.GetChildContentAsync();
+
+        // Retrieve aggregated headers and contents
+        var headers = context.Items["TabHeaders"] as List<string> ?? new List<string>();
+        var contents = context.Items["TabContents"] as List<string> ?? new List<string>();
+
+        // Render the parent container
+        output.TagName = "div";
+        output.Attributes.SetAttribute("class", "tab-container");
+
+        output.Content.SetHtmlContent($@"
+            <div class='tab-headers'>
+                {string.Join("\n", headers)}
+            </div>
+            <div class='tabs'>
+                {string.Join("\n", contents)}
+            </div>
+        ");
+    }
+}
