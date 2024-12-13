62171feb11958912b34e567f3e1274c44437f564
ColmKenna <colmkenna@gmail.com>
2024-12-13 13:04:16 +0000

    add clients list page

---
+++ src/IdentityServerAspNetIdentity/Pages/Admin/Clients/Index.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using IdentityDataModels;
+using IdentityServer.EF.DataAccess;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IdentityServerAspNetIdentity.Pages.Admin.Clients;
+
+public class Index : PageModel
+{
+    private IClientsRepository _clientsRepository;
+
+    public Index(IClientsRepository clientsRepository)
+    {
+        _clientsRepository = clientsRepository;
+    }
+
+    public IList<ClientsSummary> Clients { get; private set; }
+    
+    public async Task OnGetAsync()
+    {
+        Clients = await _clientsRepository.GetClients();
+    }
+}
