9054506194808d5d03676b9aee20edd17e9d1e2c
ColmKenna <colmkenna@gmail.com>
2024-12-13 13:04:34 +0000

    Add clients details page

---
+++ src/IdentityServerAspNetIdentity/Pages/Admin/Clients/Details.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using IdentityDataModels;
+using IdentityServer.EF.DataAccess;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IdentityServerAspNetIdentity.Pages.Admin.Clients;
+
+public class Details : PageModel
+{
+    private IClientsRepository _clientsRepository;
+
+    public Details(IClientsRepository clientsRepository)
+    {
+        _clientsRepository = clientsRepository;
+    }
+    
+    public ClientViewModel Client { get; private set; }
+    
+    public async Task OnGetAsync(string clientId)
+    {
+        Client = await _clientsRepository.GetClient(clientId);
+    }
+    
+}
