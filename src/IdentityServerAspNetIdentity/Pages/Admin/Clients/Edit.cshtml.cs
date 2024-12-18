9351d7a811e3b42ee292707e4251ed975cbf29b4
ColmKenna <colmkenna@gmail.com>
2024-12-18 12:01:57 +0000

    Add client admin page

---
+++ src/IdentityServerAspNetIdentity/Pages/Admin/Clients/Edit.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using IdentityDataModels;
+using IdentityServer.EF.DataAccess;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IdentityServerAspNetIdentity.Pages.Admin.Clients;
+
+public class Edit : PageModel
+{
+    private IClientsRepository _clientsRepository;
+    IScopesRepository _scopesRepository;
+
+    public Edit(IClientsRepository clientsRepository, IScopesRepository scopesRepository)
+    {
+        _clientsRepository = clientsRepository;
+        _scopesRepository = scopesRepository;
+    }
+    
+    public ClientViewModel Client { get; private set; }
+    public List<string> AvailableScopes { get; private set; }
+    
+    public async Task OnGetAsync(string clientId)
+    {
+        Client = await _clientsRepository.GetClient(clientId);
+        AvailableScopes = await _scopesRepository.GetScopes();
+    }
+
+}
