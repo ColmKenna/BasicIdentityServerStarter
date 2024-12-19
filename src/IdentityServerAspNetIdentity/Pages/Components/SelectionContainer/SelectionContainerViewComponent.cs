e93c002c98aab9576b28dff0ee03fd2e743ce26f
ColmKenna <43966678+ColmKenna@users.noreply.github.com>
2024-12-19 12:14:28 +0000

    Add selection container as a view component

---
+++ src/IdentityServerAspNetIdentity/Pages/Components/SelectionContainer/SelectionContainerViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityServerAspNetIdentity.Pages.Components.SelectionContainer;
+
+public class SelectionContainerViewComponent : ViewComponent
+{
+    public IViewComponentResult Invoke(string id, IEnumerable<string> source, IEnumerable<string> selected, string name, string title = "", bool allowAdd = false)
+    {
+        var model = new SelectionContainerViewModel
+        {
+            Id = id,
+            Source = source,
+            Selected = selected,
+            Name = name,
+            Title = title,
+            AllowAdd = allowAdd
+        };
+
+        return View(model);
+    }
+}
---
+++ src/IdentityServerAspNetIdentity/Pages/Components/SelectionContainer/SelectionContainerViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace IdentityServerAspNetIdentity.Pages.Components.SelectionContainer;
+
+public class SelectionContainerViewModel
+{
+    
+    public string Id { get; set; }
+    
+    public IEnumerable<string> Source { get; set; }
+
+    public IEnumerable<string> Selected { get; set; }
+
+    public string Name { get; set; }
+
+    public bool AllowAdd { get; set; }
+    
+    public string Title { get; set; } = String.Empty;
+    
+}
