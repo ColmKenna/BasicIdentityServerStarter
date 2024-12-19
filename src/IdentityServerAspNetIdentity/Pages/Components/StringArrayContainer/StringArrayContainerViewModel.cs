d0ad66c39d45dcfc6c23fd66dbda1c976ab7ea90
ColmKenna <43966678+ColmKenna@users.noreply.github.com>
2024-12-19 15:42:12 +0000

    add StringArrayContainerViewComponent

---
+++ src/IdentityServerAspNetIdentity/Pages/Components/StringArrayContainer/StringArrayContainerViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace IdentityServerAspNetIdentity.Pages.Components.StringArrayContainer;
+
+public class StringArrayContainerViewModel  
+{
+    public string Id { get; set; }
+    
+    public IEnumerable<string> Current { get; set; }
+
+    public string Name { get; set; }
+
+    public bool AllowAdd { get; set; }
+    
+    public string Title { get; set; } = String.Empty;
+    public string CssClass { get; set; } = String.Empty;
+}
