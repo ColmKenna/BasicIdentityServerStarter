98ef8a2d044a7c0d0b5b2eeb35e622ecc65ec73f
ColmKenna <43966678+ColmKenna@users.noreply.github.com>
2024-12-22 21:19:40 +0000

    function to ensure that ites are set in hidden delete list

---
+++ src/IdentityServerAspNetIdentity/wwwroot/js/forViewComponent/ToggleRemoveSelection.js
@@ -0,0 +1,14 @@
+﻿function toggleRemoveSelection(id, buttonId, deletedItemsName) {
+    var element = document.getElementById(id);
+    var button = document.getElementById(buttonId);
+    var deletedItems = document.getElementsByName(deletedItemsName)[0];
+
+
+    if (element.checked) {
+        element.checked = false;
+        button.classList.remove("delete-icon-button-selected");
+    } else {
+        element.checked = true;
+        button.classList.add("delete-icon-button-selected");
+    }
+}
