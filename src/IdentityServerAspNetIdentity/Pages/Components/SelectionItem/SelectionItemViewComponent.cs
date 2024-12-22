64609d46a5b7d1d4ecb0b4b619d3901e5b8e4b12
ColmKenna <43966678+ColmKenna@users.noreply.github.com>
2024-12-22 21:24:43 +0000

    add SelectionItemViewComponent

---
+++ src/IdentityServerAspNetIdentity/Pages/Components/SelectionItem/SelectionItemViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace IdentityServerAspNetIdentity.Pages.Components.SelectionItem;
+
+public class SelectionItemViewComponent : ViewComponent
+{
+    
+    // propertoes for name ischecked value
+    
+    public string Name { get; set; }
+    public bool IsChecked { get; set; }
+    public string Value { get; set; }
+    public bool AsRadio { get; set; }
+    
+    public IViewComponentResult Invoke(string name, bool isChecked, string value, bool asRadio = false)
+    {
+        Name = name;
+        IsChecked = isChecked;
+        Value = value;
+        AsRadio = asRadio;
+        
+        return View(this);
+    }
+  
+}
