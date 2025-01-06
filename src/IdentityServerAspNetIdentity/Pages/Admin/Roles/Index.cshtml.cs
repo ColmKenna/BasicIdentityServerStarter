7c130e2a18921ebfa95f38882a462cb285d66190
ColmKenna <43966678+ColmKenna@users.noreply.github.com>
2025-01-06 11:16:20 +0000

    Add index page for roles

---
+++ src/IdentityServerAspNetIdentity/Pages/Admin/Roles/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using IdentityServer.EF.DataAccess;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IdentityServerAspNetIdentity.Pages.Admin.Roles;
+
+public class Index : PageModel
+{
+    
+    IRolesRepository rolesRepository;
+
+    public Index(IRolesRepository rolesRepository)
+    {
+        this.rolesRepository = rolesRepository;
+    }
+    
+    
+    public List<Role> Roles { get; private set; }
+
+    public async Task OnGetAsync()
+    {
+        Roles = await rolesRepository.GetRoles();
+    }
+
+}
