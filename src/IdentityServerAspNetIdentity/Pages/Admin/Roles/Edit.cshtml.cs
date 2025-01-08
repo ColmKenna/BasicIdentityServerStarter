706f18937c1edd419450581f020aaca175a84bd5
ColmKenna <43966678+ColmKenna@users.noreply.github.com>
2025-01-08 22:36:09 +0000

    Add roles edit page

---
+++ src/IdentityServerAspNetIdentity/Pages/Admin/Roles/Edit.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using IdentityDataModels;
+using IdentityServer.EF.DataAccess;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace IdentityServerAspNetIdentity.Pages.Admin.Roles;
+
+public class Edit : PageModel
+{
+    IRolesRepository rolesRepository;
+    private IUserRepository userRepository;
+    
+    public Edit(IRolesRepository rolesRepository, IUserRepository userRepository)
+    {
+        this.rolesRepository = rolesRepository;
+        this.userRepository = userRepository;
+    }
+    
+    public Role Role { get; private set; }
+    public List<ApplicationUserSummary> Users { get; private set; }
+    public List<ApplicationUserSummary> AvailableUsers { get; set; }
+    
+    public List<SelectListItem> UserSelectListItems => AvailableUsers.Select(u => new SelectListItem(u.UserName, u.Id)).ToList();
+
+    
+    public async Task OnGetAsync(string roleName)
+    {
+        Role = await rolesRepository.GetRoleByName(roleName);
+        Users = await rolesRepository.GetUsersInRole(roleName);
+        AvailableUsers = await userRepository.GetUsers();
+
+    }
+
+}
