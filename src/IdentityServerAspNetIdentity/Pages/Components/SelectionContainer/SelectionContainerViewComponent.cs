7a09a3b6d6606f1f2c0d698b64ccab9fa05e3fe6
ColmKenna <43966678+ColmKenna@users.noreply.github.com>
2025-01-09 23:48:56 +0000

    Update selection container to use SelectionContainerItemModel for items so it has value and label

--- src/IdentityServerAspNetIdentity/Pages/Components/SelectionContainer/SelectionContainerViewComponent.cs
+++ src/IdentityServerAspNetIdentity/Pages/Components/SelectionContainer/SelectionContainerViewComponent.cs
@@ -3,13 +3,36 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace IdentityServerAspNetIdentity.Pages.Components.SelectionContainer;
 
+public class SelectionContainerItemModel
+{
+    protected bool Equals(SelectionContainerItemModel other)
+    {
+        return Id == other.Id && Value == other.Value;
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj is null) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != GetType()) return false;
+        return Equals((SelectionContainerItemModel)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Id, Value);
+    }
+
+    public string Id { get; set; }
+    public string Value { get; set; }
+}
 public class SelectionContainerViewComponent : ViewComponent
 {
-    public IViewComponentResult Invoke(string id, IEnumerable<string> source, string name,IEnumerable<string> selected = null, string cssClass="",  string title = "", bool allowAdd = false, List<SelectListItem> addFromDatasource = null,  bool asRadio = false,Dictionary<string, string> sourceDetails = null)
+    public IViewComponentResult Invoke(string id, IEnumerable<SelectionContainerItemModel> source, string name,IEnumerable<SelectionContainerItemModel> selected = null, string cssClass="",  string title = "", bool allowAdd = false, List<SelectListItem> addFromDatasource = null,  bool asRadio = false,Dictionary<string, string> sourceDetails = null)
     {
         
         sourceDetails ??= new Dictionary<string, string>();
-        selected ??= new List<string>();
+        selected ??= new List<SelectionContainerItemModel>();
         var model = new SelectionContainerViewModel
         {
             Id = id,
