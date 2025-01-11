2d8313790c3551bd2951b3029720149d1d395619
ColmKenna <43966678+ColmKenna@users.noreply.github.com>
2025-01-11 21:11:51 +0000

    Allow users to be added/removed from roles

--- src/IdentityServerAspNetIdentity/Pages/Admin/Roles/Edit.cshtml.cs
+++ src/IdentityServerAspNetIdentity/Pages/Admin/Roles/Edit.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using IdentityDataModels;
 using IdentityServer.EF.DataAccess;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
@@ -16,7 +17,10 @@ public class Edit : PageModel
         this.userRepository = userRepository;
     }
     
-    public Role Role { get; private set; }
+    [BindProperty]
+    public Role Role { get;  set; }
+
+    [BindProperty]
     public List<ApplicationUserSummary> Users { get; private set; }
     public List<ApplicationUserSummary> AvailableUsers { get; set; }
     
@@ -29,6 +33,33 @@ public class Edit : PageModel
         Users = await rolesRepository.GetUsersInRole(roleName);
         AvailableUsers = await userRepository.GetUsers();
 
+    }
+    
+    public async Task<IActionResult> OnPostAsync()
+    {
+        // read the array Users from the form
+        var selectedUsers = Request.Form["Users"].ToArray();
+        Users = await rolesRepository.GetUsersInRole(Role.Name);
+        
+        // add the selected users to the role if they are not already in the role
+        foreach (var userId in selectedUsers)
+        {
+            if (Users.All(u => u.Id != userId))
+            {
+                await userRepository.AddUserToRole(userId, Role.Name);
+            }
+        }
+        
+        // remove the users that were not selected from the role
+        foreach (var user in Users)
+        {
+            if (!selectedUsers.Contains(user.Id))
+            {
+                await userRepository.RemoveUserFromRole(user.Id, Role.Name);
+            }
+        }
+        
+        return RedirectToPage("Details", new { Role.Name });
     }
 
 }
