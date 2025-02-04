887a85ff0ca9159071af100e888d464c28f9f2f6
ColmKenna <43966678+ColmKenna@users.noreply.github.com>
2025-02-04 12:29:08 +0000

    Add extension to wrap a taghelper in a div

---
+++ RazorExtensions/HtmlContentExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace RazorExtensions;
+
+public static class HtmlContentExtensions
+{
+    /// <summary>
+    /// Wraps the current HTML content in a div with the specified CSS class.
+    /// </summary>
+    /// <param name="content">inner content.</param>
+    /// <param name="cssClass">The CSS class for the wrapper</param>
+    /// <returns>A new TagBuilder with the div wrapping the content.</returns>
+    public static TagBuilder CreateWrappedDiv(this IHtmlContent content, string cssClass)
+    {
+        ArgumentNullException.ThrowIfNull(content);
+        var wrapper = new TagBuilder("div");
+        wrapper.AddCssClassIfNotNullOrWhiteSpace(cssClass);
+        wrapper.InnerHtml.AppendHtml(content);
+        return wrapper;
+    }
+
+    /// <summary>
+    /// Adds the specified CSS class to the <see cref="TagBuilder"/> instance if the provided CSS class string is not null, empty, or composed only of white-space characters.
+    /// </summary>
+    /// <param name="content">The <see cref="TagBuilder"/> instance to which the CSS class will be added.</param>
+    /// <param name="cssClass">The CSS class to add.</param>
+    /// <returns>The updated <see cref="TagBuilder"/> instance.</returns>
+    public static TagBuilder AddCssClassIfNotNullOrWhiteSpace(this TagBuilder content, string cssClass)
+    {
+        if (!string.IsNullOrWhiteSpace(cssClass))
+        {
+            content.AddCssClass(cssClass);
+        }
+        return content;
+    }
+
+}
