8fae2df7fb26d70baf0e771d8173c3e274d5588a
colmstrata <colm@stratait.ie>
2025-02-27 17:53:27 +0000

    Create more Comprehensive ClientViewModel for tests

--- IdentityServerAspNetIdentity.UnitTests/EditPageTests.cs
+++ IdentityServerAspNetIdentity.UnitTests/EditPageTests.cs
@@ -50,7 +50,29 @@ public class EditPageTests : IClassFixture<WebApplicationFactory<Program>>
         var mockScopesRepo = new Mock<IScopesRepository>();
 
         var testClientId = "test-client";
-        var testClient = new ClientViewModel { Id = 1, ClientName = "Test Client" };
+        var testClient = new ClientViewModel
+        {
+            Id = 1,
+            ClientName = "Test Client",
+            ClientId = "test-client-id",
+            Description = "Test Description",
+            ClientUri = "http://test-client-uri",
+            AllowedGrantTypes = new List<string> { "authorization_code", "client_credentials" },
+            RequirePkce = true,
+            AllowAccessTokensViaBrowser = true,
+            RequireConsent = true,
+            AlwaysIncludeUserClaimsInIdToken = true,
+            AccessTokenLifetime = 3600,
+            AllowedScopes = new List<string> { "scope1", "scope2" },
+            RedirectUris = new List<string> { "http://redirect-uri1", "http://redirect-uri2" },
+            PostLogoutRedirectUris = new List<string> { "http://post-logout-uri1", "http://post-logout-uri2" },
+            AllowedCorsOrigins = new List<string> { "http://cors-origin1", "http://cors-origin2" },
+            ClientSecrets = new List<ClientSecretViewModel>
+            {
+                new ClientSecretViewModel { Description = "Secret1", Secret = "secret1" },
+                new ClientSecretViewModel { Description = "Secret2", Secret = "secret2" }
+            }
+        };
         var testScopes = new List<string> { "scope1", "scope2" };
 
         mockClientRepo.Setup(repo => repo.GetClient(testClientId))
